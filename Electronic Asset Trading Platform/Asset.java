e171593f1dd338c1ca7c7d77b60253724e724942
Mathew Maag <mathewmaag@gmail.com>
2021-04-16 14:20:23 +1000

    Started Java Bones

---
+++ Electronic Asset Trading Platform/Asset.java
@@ -0,0 +1,13 @@
+public class Asset {
+    private String name;
+
+    public Asset() {
+
+    }
+    public int GetQuantity() {
+        return 0;
+    }
+    public int GetQuantityListed() {
+        return 0;
+    }
+}
