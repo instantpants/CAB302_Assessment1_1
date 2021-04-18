c4769d0d15a7c1b0242680d0bcb2281d3c176518
Mathew Maag <mathewmaag@gmail.com>
2021-04-18 18:15:40 +1000

    Done some more work on classes (JavaDoc)

--- Electronic Asset Trading Platform/Asset.java
+++ Electronic Asset Trading Platform/Asset.java
@@ -1,13 +1,46 @@
-public class Asset {
-    private String name;
+/**
+ * This class stores info and methods relating to each Asset
+ *
+ * @version 0
+ * @see     Listing
+ * @see     OrganisationalUnit
+ */
+public class Asset implements Comparable<Asset>{
+    public String name;
 
-    public Asset() {
-
+    /**
+     * Creates a new Asset
+     *
+     * @param   name    name of this Asset
+     */
+    public Asset(String name) {
+        // should probably make a check that 'name' is a valid asset name
+        this.name = name;
     }
+
+    /**
+     * Gets the number of Assets the OU possesses
+     *
+     * @deprecated  assets no longer store their own quantities
+     * @return      number of units
+     */
     public int GetQuantity() {
         return 0;
     }
+
+    /**
+     * Gets the number of Assets the OU has currently listed for trading
+     *
+     * @deprecated
+     * @return      number of units
+     */
     public int GetQuantityListed() {
+        return 0;
+    }
+
+    // read JavaDoc comments for ideas on exceptions etc
+    @Override
+    public int compareTo(Asset a) {
         return 0;
     }
 }
---
+++ Electronic Asset Trading Platform/User.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+public class User {
+    private String username;
+    private OrganisationalUnit OU;
+
+    /**
+     * Creates a new user
+     *
+     * @param   username    username of this user
+     */
+    public User(String username) {
+        this.username = username;
+    }
+}
