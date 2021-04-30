dfcb2e19f6073eb3a2eb62bef4c42836c94e5c0c
Thomas <N10582835@qut.edu.au>
2021-04-30 12:14:20 +1000

    ServerConnection database v1 finished

--- Server/ServerConnection.java
+++ Server/ServerConnection.java
@@ -15,21 +15,50 @@ public class ServerConnection
             statement.setQueryTimeout(30);
 
             statement.executeUpdate("drop table if exists users");
-//            statement.executeUpdate("drop table if exists ou");
-//            statement.executeUpdate("drop table if exists assets");
-//            statement.executeUpdate("drop table if exists listings");
-//            statement.executeUpdate("drop table if exists trade_history");
+            statement.executeUpdate("drop table if exists ou");
+            statement.executeUpdate("drop table if exists assets");
+            statement.executeUpdate("drop table if exists listings");
+            statement.executeUpdate("drop table if exists trade_history");
 
             statement.executeUpdate("create table users (" +
                     "username STRING NOT NULL PRIMARY KEY, " +
                     "password STRING NOT NULL, " +
                     "account_type INT NOT NULL, " +
-                    "ou_name STRING NOT NULL)");
+                    "ou_name STRING NOT NULL" +
+                    ")"
+            );
 
-//            statement.executeUpdate("create table ou (string name, int credits)");
-//            statement.executeUpdate("create table assets (string name, string, ou, int quantity)");
-//            statement.executeUpdate("create table listings (string name, date added, int credits)");
-//            statement.executeUpdate("create table trade_history (string name, int credits)");
+            statement.executeUpdate("create table ou (" +
+                    "ou_name STRING NOT NULL PRIMARY KEY" +
+                    "credits INT NOT NULL)"
+            );
+
+            statement.executeUpdate("create table assets (" +
+                    "asset_name STRING NOT NULL, " +
+                    "ou_name STRING NOT NULL," +
+                    "quantity INT NOT NULL" +
+                    "" +
+                    "PRIMARY KEY(asset_name, ou_name)"
+            );
+
+            statement.executeUpdate("create table listings (" +
+                    "id INT NOT NULL IDENTITY PRIMARY KEY, " +
+                    "ou_name STRING NOT NULL, " +
+                    "date_added DATE NOT NULL" +
+                    "asset_name STRING NOT NULL" +
+                    "quantity INT NOT NULL" +
+                    "price INT NOT NULL" +
+                    "listing_type BIT NOT NULL)"
+            );
+
+            statement.executeUpdate("create table trade_history (" +
+                    "invoice_number INT NOT NULL IDENTITY PRIMARY KEY, " +
+                    "ou_name STRING NOT NULL" +
+                    "date_sold DATE NOT NULL" +
+                    "asset_name STRING NOT NULL" +
+                    "quantity INT NOT NULL" +
+                    "price INT NOT NULL" +
+                    "listing_type BIT NOT NULL)");
 
             statement.executeUpdate("insert into users values('root', 'pass', 0, 'IT')");
             statement.executeUpdate("insert into users values('user', 'pass', 1, 'IT')");
@@ -53,5 +82,29 @@ public class ServerConnection
                 System.err.println(e.getMessage());
             }
         }
+    }
+
+    public ResultSet QueryDatabase(String query){
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:TradingPlatform.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            return statement.executeQuery(query);
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally{
+            try {
+                if (connection != null)
+                    connection.close();
+            }catch (SQLException e){
+                System.err.println(e.getMessage());
+            }
+        }
+        return null;
     }
 }
